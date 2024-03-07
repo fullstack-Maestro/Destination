d25b04f89a9512cc263ffc42b9975d89a360af8b
Sayidahror <saidahrormirzaahmedov@gmail.com>
2024-03-07 07:36:37 +0500

    Entitities are buuilt

---
+++ Destination.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Destination.DataAccess.Configurations;
+using Destination.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destination.DataAccess.Contexts;
+
+public class AppDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(Credential.connectionString);
+    }
+    public DbSet<Card> Cards { get; set; }
+    public DbSet<MetroStation> MetroStations { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<User> Users { get; set; }
+
+}
+
--- Destination.Domain/Entities/Transaction.cs
+++ Destination.Domain/Entities/Transaction.cs
@@ -3,12 +3,9 @@
 namespace Destination.Domain.Entities;
 public class Transaction : Auditable
 {
-    public long UserId { get; set; }
-    public User User { get; set; }
     public long MetroId { get; set; }
     public MetroStation MetroStation { get; set; }
     public DateTime Time { get; set; }
     public long CardId { get; set; }
     public Card Card { get; set; }
-    public decimal Amount { get; set; } = 2000;
 }
