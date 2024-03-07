5140452acab718846967de9108750adcf6cfd5a9
Sayidahror <saidahrormirzaahmedov@gmail.com>
2024-03-07 16:28:32 +0500

    Services Added

---
+++ Destination.Service/Mappers/MapperExtension.cs
@@ -0,0 +1,23 @@
+﻿public static class MapperExtension
+{
+    public static T MapTo<T>(this object obj) where T : class, new()
+    {
+        var objType = obj.GetType();
+        var objProperties = objType.GetProperties();
+
+        var dto = new T();
+        var dtoType = dto.GetType();
+        var dtoProperties = dtoType.GetProperties();
+
+        foreach (var objProperty in objProperties)
+        {
+            if (dtoProperties.Any(p => p.Name == objProperty.Name))
+            {
+                var dtoProperty = dtoType.GetProperty(objProperty.Name);
+                var value = objProperty.GetValue(obj);
+                dtoProperty.SetValue(dto, value);
+            }
+        }
+        return dto;
+    }
+}
